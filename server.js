711191a5d53835ebe7e8ab9d6ae998eb37773684
Jose Garcia <jose@Joses-MacBook-Air.local>
2023-02-22 18:00:26 -0500

    made changes

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
-
-const express = require('express')
-const app = express ()
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoute = require('./routes/api');
-const htmlRoute = require('./routes/html');
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+const notes = path.basename("./db/db.json");
 
-app.use(express.urlencoded({ extended: true }));
+// MIDDLEWARE
 app.use(express.json());
-app.use(express.static("./develop/public"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+// HTML ROUTES CREATED
+// Returns/call the index.html home page file.
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+//Returns the notes.html file.
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
 
-app.use('/api', apiRoute);
-app.use('/', htmlRoute);
+// API ROUTES CREATED
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. NPM Package - UUID was added to each new note a unique id 
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2))
+    res.json(notes);
+});
 
-
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+// START LISTENING
+app.listen(PORT, () => console.log(' Server Listening on port ' + PORT));
